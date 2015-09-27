97e66f149f208d8ac5d67ec82afa9a77ea1ad6a0
Dantre <dantreKrt@gmail.com>
2015-09-27 13:32:58 +0600

    Start commit

---
+++ Tetris/Tetris/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class GameBoard
+    {
+        
+    }
+
+    public class Piece
+    {
+        
+
+    }
+
+    public class Cell
+    {
+        
+    }
+}
