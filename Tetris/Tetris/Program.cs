85d33207c38debe0ad4f04da6a9598a685e7a057
Dantre <dantreKrt@gmail.com>
2015-09-27 13:53:17 +0600

    Basic data classes

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Tetris
 {
@@ -13,19 +16,37 @@ namespace Tetris
         }
     }
 
+    public class JsonParser
+    {
+        private readonly string fileName;
+
+        public JsonParser(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public GameBoard Parse()
+        {
+            return JsonConvert.DeserializeObject<GameBoard>(File.ReadAllText(fileName));
+        }
+    }
+
     public class GameBoard
     {
-        
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public List<Piece> Pieces { get; set; }
+        public string Commands { get; set; }
     }
 
     public class Piece
     {
-        
-
+        public ImmutableList<Cell> Cells { get; set; }
     }
 
     public class Cell
     {
-        
+        public int X { get; set; }
+        public int Y { get; set; }
     }
 }
